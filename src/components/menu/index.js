058e51d256ab4d93807a0ece636af67224dcef78
Alice Habitzreuter <alicehabitzreuter@github.com>
2020-07-29 16:07:55 -0300

    Final da Aula do Alura, faltam ainda alguns toques

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import Logo from '../../assets/img/Logo.png'
-import './menu.css'
-import Button from '../Button/index'
+import Logo from '../../assets/img/Logo.png';
+import './menu.css';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
-function Menu (){
-    return (
-        <nav className='Menu'>
-            <a href="/">
-                <img className='Logo' src={Logo} alt='AluraFlix logo'/>
-            </a>
+function Menu() {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </a>
 
-            <Button as ='a' className='ButtonLink' href="/">
-                Novo vídeo
-            </Button>
-        </nav>
-    );
+      <Button as="a" className="ButtonLink" href="/">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
-export default Menu;
+export default Menu; 
